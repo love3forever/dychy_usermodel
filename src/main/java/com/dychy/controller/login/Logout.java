891d7a15ba48f9c38d5d875609e21e84721a07d6
love3forever <wm_gaming@163.com>
2017-01-11 10:13:40 +0800

    login&logout

--- src/main/java/com/dychy/controller/login/Logout.java
+++ src/main/java/com/dychy/controller/login/Logout.java
@@ -1,16 +1,33 @@
 package com.dychy.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by eclipse on 2017/1/10.
  */
 @Controller
 public class Logout {
-    @RequestMapping(value = "/logout")
+    @RequestMapping(value = "/logout",method = RequestMethod.POST)
     public String userLogout() {
-        return "200";
+        return null;
+    }
+
+
+    @RequestMapping(value = "/logout",method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
 
--- src/main/java/com/dychy/security/WebSecurityConfig.java
+++ src/main/java/com/dychy/security/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/","/userlogin","/register","/static/**").permitAll()
+            .antMatchers("/","/logout","/register","/static/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
